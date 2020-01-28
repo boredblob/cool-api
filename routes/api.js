c1773762d3f6455166e9f5691556226c3dd6822e
Omer <omerkaraman@hotmail.co.uk>
2020-01-28 14:21:31 +0000

    Built tree searching

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const dotenv = require("dotenv");
 const crypto = require("crypto");
+const fetch = require("node-fetch");
 dotenv.config();
 
+let tree = reloadTree().then(t => parseTree(t)).then(t => {tree = t;});
+
 router.get("/tree", async (req, res) => {
-  res.status(200).json("Woot woot");
+  res.status(200).json(tree);
 })
 
 router.get("/", async (req, res) => {
@@ -17,6 +20,7 @@ router.post("/update", async (req, res) => {
 
   if (verifySignature(data, key)) {
     console.log("Accepted");
+    tree = reloadTree().then(t => parseTree(t)).then(t => {tree = t;});
     res.status(200).send("yeet");
   } else {
     console.log("Denied");
@@ -40,6 +44,44 @@ function verifySignature(data, key) {
   catch {
     return false;
   }
+}
+
+async function reloadTree() {
+  const options = {
+    headers: {
+      "Authorisation": "token" + process.env.APP_TOKEN
+    }
+  };
+  return new Promise(resolve => {
+    fetch("https://api.github.com/repos/omerismyname/random-testing-stuff/commits/master", options)
+    .then(response => response.json())
+    .then(data => {
+      const tree_hash = data.commit.tree.sha;
+      if (tree_hash) {
+        fetch("https://api.github.com/repos/omerismyname/random-testing-stuff/git/trees/" + tree_hash + "?recursive=1", options)
+        .then(response => response.json())
+        .then(data => {
+          resolve(data.tree);
+        });
+      } else {
+        throw new Error("No tree found");
+      }
+    })
+    .catch(err => console.log("Error fetching data: " + err));
+  });
+}
+
+function parseTree(tree) {
+  const treeArray = [];
+  for (const t of tree) {
+    if (t.type === "tree") {
+      treeArray.push("index.html?dir=" + t.path);
+    }
+    if (t.type === "blob") {
+      treeArray.push(t.path);
+    }
+  }
+  return treeArray;
 }
 
 module.exports = router;
