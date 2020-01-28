4a5994f172e72cb14f691f299b505a2f99a5033f
Omer <omerkaraman@hotmail.co.uk>
2020-01-28 12:18:29 +0000

    added security on post

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const dotenv = require("dotenv");
+const crypto = require("crypto");
+dotenv.config();
 
 router.get("/tree", async (req, res) => {
   res.status(200).json("Woot woot");
@@ -7,5 +10,18 @@ router.get("/tree", async (req, res) => {
 router.get("/", async (req, res) => {
   res.status(404).send("Tree at /tree");
 })
+
+router.post("/update", async (req, res) => {
+  if (verifySignature(req.body, req.get("X-Hub-Signature"))) {
+    res.send(200);
+  } else {
+    res.send(403);
+  }  
+})
+
+function verifySignature(data, headerSignature) {
+  const signature = crypto.createHmac("sha1", process.env.SECRET_TOKEN).update(data).digest("hex");
+  return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(headerSignature));
+}
 
 module.exports = router;
